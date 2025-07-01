3f7ccb929e0785c023523e95132b01bbee939a51
Damon Fedorick <95268795+Ddemon26@users.noreply.github.com>
2025-07-01 04:14:32 -0700

    [BOT] refactor(rename): update RSImageProducer members

--- 2006Scape Client/src/main/java/RSImageProducer.java
+++ 2006Scape Client/src/main/java/RSImageProducer.java
@@ -13,77 +13,77 @@ import java.awt.image.ImageProducer;
 
 final class RSImageProducer implements ImageProducer, ImageObserver {
 
-	public RSImageProducer(int i, int j, Component component) {
-		anInt316 = i;
-		anInt317 = j;
-		anIntArray315 = new int[i * j];
-		aColorModel318 = new DirectColorModel(32, 0xff0000, 65280, 255);
-		anImage320 = component.createImage(this);
-		method239();
-		component.prepareImage(anImage320, this);
-		method239();
-		component.prepareImage(anImage320, this);
-		method239();
-		component.prepareImage(anImage320, this);
-		initDrawingArea();
-	}
+       public RSImageProducer(int width, int height, Component component) {
+               this.width = width;
+               this.height = height;
+               pixels = new int[width * height];
+               colorModel = new DirectColorModel(32, 0xff0000, 65280, 255);
+               image = component.createImage(this);
+               updateImage();
+               component.prepareImage(image, this);
+               updateImage();
+               component.prepareImage(image, this);
+               updateImage();
+               component.prepareImage(image, this);
+               initDrawingArea();
+       }
 
-	public void initDrawingArea() {
-		DrawingArea.initDrawingArea(anInt317, anInt316, anIntArray315);
-	}
+       public void initDrawingArea() {
+               DrawingArea.initDrawingArea(height, width, pixels);
+       }
 
-	public void drawGraphics(int i, Graphics g, int k) {
-		method239();
-		g.drawImage(anImage320, k, i, this);
-	}
-
-	@Override
-	public synchronized void addConsumer(ImageConsumer imageconsumer) {
-		anImageConsumer319 = imageconsumer;
-		imageconsumer.setDimensions(anInt316, anInt317);
-		imageconsumer.setProperties(null);
-		imageconsumer.setColorModel(aColorModel318);
-		imageconsumer.setHints(14);
-	}
+       public void drawGraphics(int i, Graphics g, int k) {
+               updateImage();
+               g.drawImage(image, k, i, this);
+       }
 
 	@Override
-	public synchronized boolean isConsumer(ImageConsumer imageconsumer) {
-		return anImageConsumer319 == imageconsumer;
-	}
+       public synchronized void addConsumer(ImageConsumer imageconsumer) {
+               this.imageConsumer = imageconsumer;
+               imageconsumer.setDimensions(width, height);
+               imageconsumer.setProperties(null);
+               imageconsumer.setColorModel(colorModel);
+               imageconsumer.setHints(14);
+       }
 
 	@Override
-	public synchronized void removeConsumer(ImageConsumer imageconsumer) {
-		if (anImageConsumer319 == imageconsumer) {
-			anImageConsumer319 = null;
-		}
-	}
+       public synchronized boolean isConsumer(ImageConsumer imageconsumer) {
+               return this.imageConsumer == imageconsumer;
+       }
 
 	@Override
-	public void startProduction(ImageConsumer imageconsumer) {
-		addConsumer(imageconsumer);
-	}
+       public synchronized void removeConsumer(ImageConsumer imageconsumer) {
+               if (this.imageConsumer == imageconsumer) {
+                       this.imageConsumer = null;
+               }
+       }
 
 	@Override
-	public void requestTopDownLeftRightResend(ImageConsumer imageconsumer) {
-		System.out.println("TDLR");
-	}
+       public void startProduction(ImageConsumer imageconsumer) {
+               addConsumer(imageconsumer);
+       }
 
-	private synchronized void method239() {
-		if (anImageConsumer319 != null) {
-			anImageConsumer319.setPixels(0, 0, anInt316, anInt317, aColorModel318, anIntArray315, 0, anInt316);
-			anImageConsumer319.imageComplete(2);
-		}
-	}
+	@Override
+       public void requestTopDownLeftRightResend(ImageConsumer imageconsumer) {
+               System.out.println("TDLR");
+       }
+
+       private synchronized void updateImage() {
+               if (imageConsumer != null) {
+                       imageConsumer.setPixels(0, 0, width, height, colorModel, pixels, 0, width);
+                       imageConsumer.imageComplete(2);
+               }
+       }
 
 	@Override
 	public boolean imageUpdate(Image image, int i, int j, int k, int l, int i1) {
 		return true;
 	}
 
-	public final int[] anIntArray315;
-	private final int anInt316;
-	private final int anInt317;
-	private final ColorModel aColorModel318;
-	private ImageConsumer anImageConsumer319;
-	private final Image anImage320;
+       public final int[] pixels;
+       private final int width;
+       private final int height;
+       private final ColorModel colorModel;
+       private ImageConsumer imageConsumer;
+       private final Image image;
 }
