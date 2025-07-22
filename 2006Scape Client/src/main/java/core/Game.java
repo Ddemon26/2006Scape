d630625a42b543a113cf7443c034c19be809acbb
Damon Fedorick <95268795+Ddemon26@users.noreply.github.com>
2025-07-22 01:49:19 -0700

    [BOT]: refactor(camera): extract camera manager

--- 2006Scape Client/src/main/java/core/Game.java
+++ 2006Scape Client/src/main/java/core/Game.java
@@ -67,6 +67,7 @@ import core.MinimapRenderer;
 import core.GroundItemSpawner;
 import core.MenuManager;
 import render.Background;
+import core.CameraManager;
 import render.BoundaryObject;
 import render.CollisionMap;
 import render.DrawingArea;
@@ -1315,100 +1316,9 @@ public void drawChatArea() {
 
 	}
 
-	public void calcCameraPos() {
-		int i = cameraTargetX * 128 + 64;
-		int j = cameraTargetY * 128 + 64;
-		int k = getTileHeight(plane, j, i) - cameraTargetZ;
-		if (xCameraPos < i) {
-			xCameraPos += cameraMoveSpeed + (i - xCameraPos) * cameraMoveAcceleration / 1000;
-			if (xCameraPos > i) {
-				xCameraPos = i;
-			}
-		}
-		if (xCameraPos > i) {
-			xCameraPos -= cameraMoveSpeed + (xCameraPos - i) * cameraMoveAcceleration / 1000;
-			if (xCameraPos < i) {
-				xCameraPos = i;
-			}
-		}
-		if (zCameraPos < k) {
-			zCameraPos += cameraMoveSpeed + (k - zCameraPos) * cameraMoveAcceleration / 1000;
-			if (zCameraPos > k) {
-				zCameraPos = k;
-			}
-		}
-		if (zCameraPos > k) {
-			zCameraPos -= cameraMoveSpeed + (zCameraPos - k) * cameraMoveAcceleration / 1000;
-			if (zCameraPos < k) {
-				zCameraPos = k;
-			}
-		}
-		if (yCameraPos < j) {
-			yCameraPos += cameraMoveSpeed + (j - yCameraPos) * cameraMoveAcceleration / 1000;
-			if (yCameraPos > j) {
-				yCameraPos = j;
-			}
-		}
-		if (yCameraPos > j) {
-			yCameraPos -= cameraMoveSpeed + (yCameraPos - j) * cameraMoveAcceleration / 1000;
-			if (yCameraPos < j) {
-				yCameraPos = j;
-			}
-		}
-		i = cameraFocusX * 128 + 64;
-		j = cameraFocusY * 128 + 64;
-		k = getTileHeight(plane, j, i) - cameraFocusHeight;
-		int l = i - xCameraPos;
-		int i1 = k - zCameraPos;
-		int j1 = j - yCameraPos;
-		int k1 = (int) Math.sqrt(l * l + j1 * j1);
-		int l1 = (int) (Math.atan2(i1, k1) * 325.94900000000001D) & 0x7ff;
-		int i2 = (int) (Math.atan2(l, j1) * -325.94900000000001D) & 0x7ff;
-		if (l1 < 128) {
-			l1 = 128;
-		}
-		if (l1 > 383) {
-			l1 = 383;
-		}
-		if (yCameraCurve < l1) {
-			yCameraCurve += cameraAdjustSpeed + (l1 - yCameraCurve) * cameraAdjustAcceleration / 1000;
-			if (yCameraCurve > l1) {
-				yCameraCurve = l1;
-			}
-		}
-		if (yCameraCurve > l1) {
-			yCameraCurve -= cameraAdjustSpeed + (yCameraCurve - l1) * cameraAdjustAcceleration / 1000;
-			if (yCameraCurve < l1) {
-				yCameraCurve = l1;
-			}
-		}
-		int j2 = i2 - xCameraCurve;
-		if (j2 > 1024) {
-			j2 -= 2048;
-		}
-		if (j2 < -1024) {
-			j2 += 2048;
-		}
-		if (j2 > 0) {
-			xCameraCurve += cameraAdjustSpeed + j2 * cameraAdjustAcceleration / 1000;
-			xCameraCurve &= 0x7ff;
-		}
-		if (j2 < 0) {
-			xCameraCurve -= cameraAdjustSpeed + -j2 * cameraAdjustAcceleration / 1000;
-			xCameraCurve &= 0x7ff;
-		}
-		int k2 = i2 - xCameraCurve;
-		if (k2 > 1024) {
-			k2 -= 2048;
-		}
-		if (k2 < -1024) {
-			k2 += 2048;
-		}
-		if (k2 < 0 && j2 > 0 || k2 > 0 && j2 < 0) {
-			xCameraCurve = i2;
-		}
-	}
-
+        public void calcCameraPos() {
+                cameraManager.calcCameraPos();
+        }
         public void drawMenu() {
                 menuManager.drawMenu();
         }
@@ -7819,102 +7729,9 @@ public void drawChatArea() {
 		}
 	}
 
-       public void updateCameraPosition() {
-		try {
-			int j = myPlayer.x + cameraXOffset;
-			int k = myPlayer.y + cameraYOffset;
-			if (cameraX - j < -500 || cameraX - j > 500 || cameraY - k < -500 || cameraY - k > 500) {
-				cameraX = j;
-				cameraY = k;
-			}
-			if (cameraX != j) {
-				cameraX += (j - cameraX) / 16;
-			}
-			if (cameraY != k) {
-				cameraY += (k - cameraY) / 16;
-			}
-                        if (super.keyArray[1] == 1) {
-                                cameraYawAccel += (-24 - cameraYawAccel) / 2;
-                        } else if (super.keyArray[2] == 1) {
-                                cameraYawAccel += (24 - cameraYawAccel) / 2;
-                        } else {
-                                cameraYawAccel /= 2;
-                        }
-                        if (super.keyArray[3] == 1) {
-                                cameraPitchAccel += (12 - cameraPitchAccel) / 2;
-                        } else if (super.keyArray[4] == 1) {
-                                cameraPitchAccel += (-12 - cameraPitchAccel) / 2;
-                        } else {
-                                cameraPitchAccel /= 2;
-                        }
-                        cameraYaw = cameraYaw + cameraYawAccel / 2 & 0x7ff;
-                        cameraPitch += cameraPitchAccel / 2;
-			if (cameraPitch < 128) {
-				cameraPitch = 128;
-			}
-			if (cameraPitch > 383) {
-				cameraPitch = 383;
-			}
-			int l = cameraX >> 7;
-			int i1 = cameraY >> 7;
-			int j1 = getTileHeight(plane, cameraY, cameraX);
-			int k1 = 0;
-			if (l > 3 && i1 > 3 && l < 100 && i1 < 100) {
-				for (int l1 = l - 4; l1 <= l + 4; l1++) {
-					for (int k2 = i1 - 4; k2 <= i1 + 4; k2++) {
-						int l2 = plane;
-						if (l2 < 3 && (tileFlags[1][l1][k2] & 2) == 2) {
-							l2++;
-						}
-						int i3 = j1 - tileHeights[l2][l1][k2];
-						if (i3 > k1) {
-							k1 = i3;
-						}
-					}
-
-				}
-
-			}
-			cameraMoveCycle++;
-			if (cameraMoveCycle > 1512) {
-				cameraMoveCycle = 0;
-				stream.createFrame(77);
-				stream.writeWordBigEndian(0);
-				int i2 = stream.currentOffset;
-				stream.writeWordBigEndian((int) (Math.random() * 256D));
-				stream.writeWordBigEndian(101);
-				stream.writeWordBigEndian(233);
-				stream.writeWord(45092);
-				if ((int) (Math.random() * 2D) == 0) {
-					stream.writeWord(35784);
-				}
-				stream.writeWordBigEndian((int) (Math.random() * 256D));
-				stream.writeWordBigEndian(64);
-				stream.writeWordBigEndian(38);
-				stream.writeWord((int) (Math.random() * 65536D));
-				stream.writeWord((int) (Math.random() * 65536D));
-				stream.writeBytes(stream.currentOffset - i2);
-			}
-			int j2 = k1 * 192;
-			if (j2 > 0x17f00) {
-				j2 = 0x17f00;
-			}
-			if (j2 < 32768) {
-				j2 = 32768;
-			}
-			if (j2 > cameraZoom) {
-				cameraZoom += (j2 - cameraZoom) / 24;
-				return;
-			}
-			if (j2 < cameraZoom) {
-				cameraZoom += (j2 - cameraZoom) / 80;
-			}
-		} catch (Exception _ex) {
-			Signlink.reporterror("glfc_ex " + myPlayer.x + "," + myPlayer.y + "," + cameraX + "," + cameraY + "," + currentRegionX + "," + currentRegionY + "," + baseX + "," + baseY);
-			throw new RuntimeException("eek");
-		}
-	}
-
+        public void updateCameraPosition() {
+                cameraManager.updateCameraPosition();
+        }
 	public void processDrawing() {
 		if (rsAlreadyLoaded || loadingError || genericLoadingError) {
 			showErrorScreen();
@@ -8230,85 +8047,8 @@ public void drawChatArea() {
 	}
 
        public int determineCameraPlane() {
-		int j = 3;
-		if (yCameraCurve < 310) {
-			int k = Math.max(0, Math.min(103, xCameraPos >> 7));
-			int l = Math.max(0, Math.min(103, yCameraPos >> 7));
-			int i1 = myPlayer.x >> 7;
-			int j1 = myPlayer.y >> 7;
-			if ((tileFlags[plane][k][l] & 4) != 0) {
-				j = plane;
-			}
-			int k1;
-			if (i1 > k) {
-				k1 = i1 - k;
-			} else {
-				k1 = k - i1;
-			}
-			int l1;
-			if (j1 > l) {
-				l1 = j1 - l;
-			} else {
-				l1 = l - j1;
-			}
-			if (k1 > l1) {
-				int i2 = l1 * 0x10000 / k1;
-				int k2 = 32768;
-				while (k != i1) {
-					if (k < i1) {
-						k++;
-					} else if (k > i1) {
-						k--;
-					}
-					if ((tileFlags[plane][k][l] & 4) != 0) {
-						j = plane;
-					}
-					k2 += i2;
-					if (k2 >= 0x10000) {
-						k2 -= 0x10000;
-						if (l < j1) {
-							l++;
-						} else if (l > j1) {
-							l--;
-						}
-						if ((tileFlags[plane][k][l] & 4) != 0) {
-							j = plane;
-						}
-					}
-				}
-			} else {
-				int j2 = k1 * 0x10000 / l1;
-				int l2 = 32768;
-				while (l != j1) {
-					if (l < j1) {
-						l++;
-					} else if (l > j1) {
-						l--;
-					}
-					if ((tileFlags[plane][k][l] & 4) != 0) {
-						j = plane;
-					}
-					l2 += j2;
-					if (l2 >= 0x10000) {
-						l2 -= 0x10000;
-						if (k < i1) {
-							k++;
-						} else if (k > i1) {
-							k--;
-						}
-						if ((tileFlags[plane][k][l] & 4) != 0) {
-							j = plane;
-						}
-					}
-				}
-			}
-		}
-		if ((tileFlags[plane][myPlayer.x >> 7][myPlayer.y >> 7] & 4) != 0) {
-			j = plane;
-		}
-		return j;
-	}
-
+                return cameraManager.determineCameraPlane();
+        }
        public int getCurrentPlane() {
 		// Hide other planes when using fixed camera
 		return plane;
@@ -9290,38 +9030,9 @@ public void drawChatArea() {
 
 	}
 
-	public void setCameraPos(int j, int k, int l, int i1, int j1, int k1) {
-		int l1 = 2048 - k & 0x7ff;
-		int i2 = 2048 - j1 & 0x7ff;
-		int j2 = 0;
-		int k2 = 0;
-		int l2 = j;
-		if (l1 != 0) {
-			int i3 = Model.sineTable[l1];
-			int k3 = Model.cosineTable[l1];
-			int i4 = k2 * k3 - l2 * i3 >> 16;
-			l2 = k2 * i3 + l2 * k3 >> 16;
-			k2 = i4;
-		}
-		if (i2 != 0) {
-			/*
-			 * xxx if(cameratoggle){ if(zoom == 0) zoom = k2; if(lftrit == 0)
-			 * lftrit = j2; if(fwdbwd == 0) fwdbwd = l2; k2 = zoom; j2 = lftrit;
-			 * l2 = fwdbwd; }
-			 */
-			int j3 = Model.sineTable[i2];
-			int l3 = Model.cosineTable[i2];
-			int j4 = l2 * j3 + j2 * l3 >> 16;
-			l2 = l2 * l3 - j2 * j3 >> 16;
-			j2 = j4;
-		}
-		xCameraPos = l - j2;
-		zCameraPos = i1 - k2;
-		yCameraPos = k1 - l2;
-		yCameraCurve = k;
-		xCameraCurve = j1;
-	}
-
+   public void setCameraPos(int j, int k, int l, int i1, int j1, int k1) {
+        cameraManager.setCameraPos(j, k, l, i1, j1, k1);
+    }
 	public boolean parsePacket() {
 		if (socketStream == null) {
 			return false;
@@ -10906,6 +10617,7 @@ public void drawChatArea() {
                 menuManager = new MenuManager(this);
                 friendManager = new FriendManager(this);
                 ignoreManager = new IgnoreManager(this);
+                cameraManager = new CameraManager(this);
         }
 	public CRC32 fileCRC;
 	public static String server;
@@ -11327,6 +11039,7 @@ public void drawChatArea() {
         public final MinimapRenderer minimapRenderer;
         public final GroundItemSpawner groundItemSpawner;
         public final MenuManager menuManager;
+        public final CameraManager cameraManager;
         public int flameOffset;
 	public int backDialogID;
 	public int cameraXOffset;
---
+++ docs/Client/classes/CameraManager.md
@@ -0,0 +1,15 @@
+# CameraManager
+
+Defined in [`2006Scape Client/src/main/java/core/CameraManager.java`](../../2006Scape%20Client/src/main/java/core/CameraManager.java).
+
+Handles camera movement and orientation for `Game`.
+
+```java
+public class CameraManager {
+    public CameraManager(Game game)
+    public void calcCameraPos()
+    public void updateCameraPosition()
+    public int determineCameraPlane()
+    public void setCameraPos(int j, int k, int l, int i1, int j1, int k1)
+}
+```
